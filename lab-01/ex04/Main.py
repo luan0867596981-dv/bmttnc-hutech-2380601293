1e9f39f5e531df9b35acd72493631f9027cabd37
Luan <luan0867596981@gmail.com>
2026-05-22 08:36:46 +0700

    update code

--- lab-01/ex04/Main.py
+++ lab-01/ex04/Main.py
@@ -1,77 +1,100 @@
-from QuanLySinhVien import QuanLySinhVien   
+from QuanLySinhVien import QuanLySinhVien
+
 qlsv = QuanLySinhVien()
-while (1==1):
-    print("\n CHUONG TRINH QUAN LY SINH VIEN")
-    print("**************************Menu**************************")
-    print("** 1.Them sinh vien.                                  **")
-    print("** 2.Cap nhat thong tin sinh vien boi ID.             **")
-    print("** 3.Xoa sinh vien boi ID.                            **")
-    print("** 4.Tim kiem sinh vien theo ten.                     **")
-    print("** 5.Sap xep sinh vien theo diem trung binh.          **")
-    print("** 6.Sap xep sinh vien theo ten chuyen nghanh.        **")
-    print("** 7.Hien thi danh sach sinh vien.                    **")
-    print("** 0.Thoat.                                           **")
-    print("******************************************************")
-     
-    key = int(input("Nhap lua chon cua ban: "))
-    if (key ==1):
-        print("\n 1.Them sinh vien.")
+
+while True:
+
+    print("\nCHUONG TRINH QUAN LY SINH VIEN")
+    print("************************** MENU **************************")
+    print("**               1. Them sinh vien                      **")
+    print("**               2. Cap nhat sinh vien theo ID          **")
+    print("**               3. Xoa sinh vien theo ID               **")
+    print("**               4. Tim kiem sinh vien theo ten         **")
+    print("**               5. Sap xep sinh vien theo diem TB      **")
+    print("**               6. Sap xep sinh vien theo ten          **")
+    print("**               7. Hien thi danh sach sinh vien        **")
+    print("**               0. Thoat                               **")
+    print("**********************************************************")
+
+    key = int(input("Nhap lua chon: "))
+
+    # Thêm sinh viên
+    if key == 1:
+        print("\nTHEM SINH VIEN")
         qlsv.nhapSinhVien()
-        print("Sinh vien da duoc them vao danh sach.")
-    elif (key == 2):
-        if(qlsv.soLuongSinhVien() > 0):
-            print("\n 2.Cap nhat thong tin sinh vien boi ID.")
-            print("Nhap ID sinh vien can cap nhat: ")
-            ID = int(input())
+
+    # Cập nhật
+    elif key == 2:
+        if qlsv.soLuongSinhVien() > 0:
+
+            ID = int(input("Nhap ID sinh vien can sua: "))
             qlsv.updateSinhVien(ID)
+
         else:
-            print("Danh sach sinh vien rong.")
-    elif (key == 3):
-        if(qlsv.soLuongSinhVien() > 0):
-            print("\n 3.Xoa sinh vien boi ID.")
-            print("Nhap ID sinh vien can xoa: ")
-            ID = int(input())
-            if (qlsv.deleteByID(ID)):
-                print("Sinh vien co ID = ", ID, " da duoc xoa khoi danh sach.")
+            print("Danh sach rong!")
+
+    # Xóa
+    elif key == 3:
+        if qlsv.soLuongSinhVien() > 0:
+
+            ID = int(input("Nhap ID sinh vien can xoa: "))
+
+            if qlsv.deleteByID(ID):
+                print("Xoa thanh cong!")
             else:
-                print("Sinh vien co ID = ", ID, " khong ton tai trong danh sach.")
+                print("Khong tim thay sinh vien!")
+
         else:
-            print("Danh sach sinh vien rong.")
-            
-    elif (key == 4):
-        if(qlsv.soLuongSinhVien() > 0):
-            print("\n 4.Tim kiem sinh vien theo ten.")
-            print("Nhap tu khoa tim kiem: ")
-            name = input()
-            seachResult = qlsv.findByName(name)
-            qlsv.showSinhVien(seachResult)   
+            print("Danh sach rong!")
+
+    # Tìm kiếm
+    elif key == 4:
+        if qlsv.soLuongSinhVien() > 0:
+
+            keyword = input("Nhap ten can tim: ")
+
+            result = qlsv.findByName(keyword)
+
+            qlsv.showSinhVien(result)
+
         else:
-            print("Danh sach sinh vien rong.")  
-    
-    elif (key == 5):
-        if(qlsv.soLuongSinhVien() > 0):
-            print("\n 5.Sap xep sinh vien theo diem trung binh.")
+            print("Danh sach rong!")
+
+    # Sắp xếp điểm TB
+    elif key == 5:
+        if qlsv.soLuongSinhVien() > 0:
+
             qlsv.sortByDiemTB()
-            qlsv.showSinhVien(qlsv.listSinhVien)
+
+            qlsv.showSinhVien(qlsv.getListSinhVien())
+
         else:
-            print("Danh sach sinh vien rong.")  
-            
-    elif (key == 6):
-        if(qlsv.soLuongSinhVien() > 0):
-            print("\n 6.Sap xep sinh vien theo ten chuyen nghanh.")
+            print("Danh sach rong!")
+
+    # Sắp xếp tên
+    elif key == 6:
+        if qlsv.soLuongSinhVien() > 0:
+
             qlsv.sortByName()
-            qlsv.showSinhVien(qlsv.listSinhVien)
+
+            qlsv.showSinhVien(qlsv.getListSinhVien())
+
         else:
-            print("Danh sach sinh vien rong.")
-    elif (key == 7):
-        if(qlsv.soLuongSinhVien() > 0):
-            print("\n 7.Hien thi danh sach sinh vien.")
-            qlsv.showSinhVien(qlsv.listSinhVien)
+            print("Danh sach rong!")
+
+    # Hiển thị danh sách
+    elif key == 7:
+        if qlsv.soLuongSinhVien() > 0:
+
+            qlsv.showSinhVien(qlsv.getListSinhVien())
+
         else:
-            print("Danh sach sinh vien rong.")
-    elif (key == 0):
-        print("Thoat chuong trinh.")
+            print("Danh sach rong!")
+
+    # Thoát
+    elif key == 0:
+        print("Thoat chuong trinh!")
         break
+
     else:
-        print("Lua chon khong hop le. Vui long chon lai.")
-        print("\n Hay chon chuc nang trong truong hop menu")
+        print("Lua chon khong hop le!")
--- lab-01/ex04/QuanLySinhVien.py
+++ lab-01/ex04/QuanLySinhVien.py
@@ -1,93 +1,136 @@
 from SinhVien import SinhVien
 
-class QuaLySinhVien:
-    listSinhVien = []
-    
+
+class QuanLySinhVien:
+    def __init__(self):
+        self.listSinhVien = []
+
+    # Sinh ID tự tăng
     def generateID(self):
         maxID = 1
-        if (self.soLuongSinhVien() > 0):
+
+        if len(self.listSinhVien) > 0:
             maxID = self.listSinhVien[0]._id
+
             for sv in self.listSinhVien:
-                if (sv._id > maxID):
+                if sv._id > maxID:
                     maxID = sv._id
+
             maxID += 1
+
         return maxID
-    
+
+    # Số lượng sinh viên
     def soLuongSinhVien(self):
+        return len(self.listSinhVien)
+
+    # Thêm sinh viên
+    def nhapSinhVien(self):
         svID = self.generateID()
+
         name = input("Nhập tên sinh viên: ")
         sex = input("Nhập giới tính sinh viên: ")
         major = input("Nhập ngành học sinh viên: ")
         diemTB = float(input("Nhập điểm trung bình sinh viên: "))
+
         sv = SinhVien(svID, name, sex, major, diemTB)
+
         self.xepLoaiHocLuc(sv)
+
         self.listSinhVien.append(sv)
-        
+
+    # Cập nhật sinh viên
     def updateSinhVien(self, ID):
-        sv: SinhVien = self.findByID(ID)
-        if (sv != None):
-                name = input("Nhập tên sinh viên: ")
-                sex = input("Nhập giới tính sinh viên: ")
-                major = input("Nhập ngành học sinh viên: ")
-                diemTB = float(input("Nhập điểm trung bình sinh viên: "))
-                sv._name = name
-                sv._sex = sex
-                sv._major = major
-                sv._diemTB = diemTB
-                self.xepLoaiHocLuc(sv)
-        else :
-            print("Sinh viên có ID = {} không tồn tại".format(ID))
-    
+        sv = self.findByID(ID)
+
+        if sv is not None:
+            sv._name = input("Nhập tên sinh viên: ")
+            sv._sex = input("Nhập giới tính sinh viên: ")
+            sv._major = input("Nhập ngành học sinh viên: ")
+            sv._diemTB = float(input("Nhập điểm trung bình sinh viên: "))
+
+            self.xepLoaiHocLuc(sv)
+
+            print("Cập nhật thành công!")
+        else:
+            print("Sinh viên không tồn tại!")
+
+    # Xóa sinh viên
+    def deleteByID(self, ID):
+        sv = self.findByID(ID)
+
+        if sv is not None:
+            self.listSinhVien.remove(sv)
+            return True
+
+        return False
+
+    # Tìm theo ID
+    def findByID(self, ID):
+        for sv in self.listSinhVien:
+            if sv._id == ID:
+                return sv
+
+        return None
+
+    # Tìm theo tên
+    def findByName(self, keyword):
+        result = []
+
+        for sv in self.listSinhVien:
+            if keyword.upper() in sv._name.upper():
+                result.append(sv)
+
+        return result
+
+    # Sắp xếp theo ID
     def sortByID(self):
-        self.listSinhVien.sort(key=lambda x: x._id, reverse=False)
-    
+        self.listSinhVien.sort(key=lambda x: x._id)
+
+    # Sắp xếp theo tên
     def sortByName(self):
-        self.listSinhVien.sort(key=lambda x: x._name, reverse=False)
-        
+        self.listSinhVien.sort(key=lambda x: x._name)
+
+    # Sắp xếp theo điểm TB
     def sortByDiemTB(self):
         self.listSinhVien.sort(key=lambda x: x._diemTB, reverse=True)
-    
-    def findByID(self, ID):
-        searchResult = None
-        if (self.soLuongSinhVien() > 0):
-            for sv in self.listSinhVien:
-                if (sv._id == ID):
-                    searchResult = sv
-        return searchResult
-    
-    def findByName(self, keyword):
-        listSV = []
-        if (self.soLuongSinhVien() > 0):
-            for sv in self.listSinhVien:
-                if (keyword.upper() in sv._name.upper()):
-                    listSV.append(sv)
-        return listSV
-    
-    def deleteByID(self, ID):
-        isDeleted = False
-        sv = self.findByID(ID)
-        if (sv != None):
-            self.listSinhVien.remove(sv)
-            isDeleted = True
-        return isDeleted
-    
-    def xepLoaiHocLuc(self, sv: SinhVien):
-        if (sv._diemTB >= 8):
+
+    # Xếp loại học lực
+    def xepLoaiHocLuc(self, sv):
+        if sv._diemTB >= 8:
             sv._hocluc = "Giỏi"
-        elif (sv._diemTB >= 6.5):
+        elif sv._diemTB >= 6.5:
             sv._hocluc = "Khá"
-        elif (sv._diemTB >= 5):
-            sv._hocluc = "Trung bình"
+        elif sv._diemTB >= 5:
+            sv._hocluc = "Trung Bình"
         else:
             sv._hocluc = "Yếu"
-    
+
+    # Hiển thị danh sách sinh viên
     def showSinhVien(self, listSV):
-        print("{:<=8} {:<=18} {:<=8} {:<=8} {:<=8} {:<=8}".format("ID", "Name", "Sex", "Major", "DiemTB", "HocLuc"))
-        if (listSV)._len_() > 0:
-            for sv in listSV:
-                print("{:<=8} {:<=18} {:<=8} {:<=8} {:<=8} {:<=8}".format(sv._id, sv._name, sv._sex, sv._major, sv._diemTB, sv._hocluc))
-        print("\n")
-    
-    
+        print(
+            "{:<8} {:<20} {:<10} {:<15} {:<10} {:<10}".format(
+                "ID",
+                "Name",
+                "Sex",
+                "Major",
+                "DiemTB",
+                "HocLuc"
+            )
+        )
+
+        for sv in listSV:
+            print(
+                "{:<8} {:<20} {:<10} {:<15} {:<10} {:<10}".format(
+                    sv._id,
+                    sv._name,
+                    sv._sex,
+                    sv._major,
+                    sv._diemTB,
+                    sv._hocluc
+                )
+            )
+
+    # Lấy danh sách sinh viên
     def getListSinhVien(self):
         return self.listSinhVien
